db7890e1d6531a5b249407b40444102bcd9e8da3
Rik Meijer <rikmeijer@gmail.com>
2016-09-29 13:42:08 +0200

    add velden implementatie

--- src/domain/field.d
+++ src/domain/field.d
@@ -1,15 +1,70 @@
 module sokoban.domain.field;
 
 import sokoban.domain.stone;
+import std.algorithm.searching;
+import std.algorithm.mutation;
 
-interface Field {
+interface Veld {
 	
-	public void putStone(Stone stone);
-	public void removeStone(Stone stone);
-	public Field next();
+	public bool putStone(Stone stone);
+	public bool removeStone(Stone stone);
 	
 }
 
-class CollectionField : Field {
+class Normaal : Veld {
 	
+	private Stone stone;
+	
+	public bool putStone(Stone stone) {
+		// TODO: sla steen
+		
+		this.stone = stone;
+		return true;
+	}
+	public bool removeStone(Stone stone) {
+		if (stone != this.stone) {
+			return false;
+		}
+		this.stone = null;
+		return true;
+	}
+
+}
+
+class Uiteinde : Veld {
+	
+	private Stone[] stones;
+	
+	public bool putStone(Stone stone) {
+		this.stones ~= stone;
+		return true;
+	}
+	public bool removeStone(Stone stone) {
+		if (canFind(this.stones, stone) == false) {
+			return false;
+		}
+		
+		remove!(e => e == stone)(this.stones);
+		return true;
+	}
+}
+
+class Rozet : Veld {
+	
+	private Stone stone;
+	
+	public bool putStone(Stone stone) {
+		if (this.stone !is null) {
+			return false;
+		}
+		this.stone = stone;
+		return true;
+	}
+	public bool removeStone(Stone stone) {
+		if (stone != this.stone) {
+			return false;
+		}
+		this.stone = null;
+		return true;
+	}
 }
